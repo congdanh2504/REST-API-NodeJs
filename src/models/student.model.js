e7884d98ad55c0d40424fb535bc9eb9b4900a522
congdanh2504 <danhuchiha2002@gmail.com>
2021-06-12 13:55:55 +0700

    fix

--- src/models/student.model.js
+++ src/models/student.model.js
@@ -10,7 +10,7 @@ Student.getAllStudents = (result) => {
     dbConn.query('SELECT * FROM sinhvien', (err, res) => {
         if (err) {
             console.log('Error while fetching', err);
-            result(null, err);
+            result(err, null);
         } else {
             console.log('Students fetched successfully');
             result(null, res);
@@ -22,7 +22,7 @@ Student.getStudentByID = (id, result) => {
     dbConn.query('SELECT * FROM sinhvien WHERE ID=?', id, (err, res) => {
         if (err) {
             console.log('Error while fetching by id', err);
-            result(null, err);
+            result(err, null);
         } else {
             console.log('Students fetched by id successfully');
             result(null, res);
@@ -34,7 +34,7 @@ Student.createNewStudent = (studentData, result) => {
     dbConn.query('INSERT INTO sinhvien SET ?', studentData, (err, res) => {
         if (err) {
             console.log('Error while insert');
-            result(null, err);
+            result(err, null);
         } else {
             console.log('Student created successfully');
             result(null, res);
@@ -47,7 +47,7 @@ Student.updateStudent = (id, studentData, result) => {
      [studentData.hoten, studentData.namsinh, studentData.diachi,id], (err, res) => {
         if (err) {
             console.log('Error while updating');
-            result(null, err)
+            result(err, null)
         } else {
             console.log('Student updated');
             result(null, res);
@@ -60,7 +60,7 @@ Student.deleteStudent = (id, result) => {
     dbConn.query('DELETE FROM sinhvien WHERE id = ?',id, (err, res) => {
         if (err) {
             console.log('Error while deleting');
-            result(null, err)
+            result(err, null)
         } else {
             console.log('Student deleted');
             result(null, res);
